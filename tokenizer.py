b30b56915fa2452b47a1840cf8d09790fdd1756e
surajsedai <surajsedai05@gmail.com>
2025-11-19 22:25:30 -0500

    pair_frequency

--- tokenizer.py
+++ tokenizer.py
@@ -7,7 +7,13 @@ class BPETokenizer:
 
     def train(self, text):
         # will implement BPE training here
-        pass
+        text.lower()
+        word = text.strip().split()
+        tokens_list = [self._word_to_chars(w) for w in word]
+
+    def _word_to_chars(self, word):
+        # turn a word into a list of char
+        return list(word)
 
     def encode(self, text):
         # string -> token ids
@@ -16,3 +22,17 @@ class BPETokenizer:
     def decode(self, token_ids):
         # token ids -> string
         pass
+
+    def get_pair_frequencies(self, tokens_list):
+        # get frequencies of adjacent token pairs
+        pair_counts = dict()
+        for token_list in tokens_list:
+            for i in range(len(token_list-2)):
+                pair = (token_list[i], token_list[i+1])
+
+                if pair not in pair_counts:
+                    pair_counts[pair] = 1
+                else:
+                    pair_counts[pair] +=1
+        
+        return pair_counts
